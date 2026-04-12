6b6ff59c77cda6d25dfd0d62f379612a7350fe6b
Jhunriz <lalata.jhunriz.bscs2019@gmail.com>
2026-04-12 16:05:56 +0800

    feat: update frontend dependencies and add button component
    - Added new dependencies: @base-ui/react, @fontsource-variable/instrument-sans, @fontsource-variable/inter, class-variance-authority, clsx, lucide-react, shadcn, tailwind-merge, and tw-animate-css.
    - Updated App component to include a counter with increment and decrement buttons.
    - Enhanced index.css with custom styles and theme variables.
    - Configured TypeScript paths for easier imports.
    - Updated Vite configuration to support aliasing for src directory.
    - Created a new button component with variants using class-variance-authority.
    - Added utility function for merging class names.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
 import './App.css'
+import { Button } from './components/ui/button'
+
 
 function App() {
-
-
+  const [click, setClick] = useState<number>(0);
   return (
     <>
-      <h1 className='text-green-500 text-5xl'>asdfsdf</h1>
+      <div className='flex justify-center items-center h-screen'>
+        <div className='flex flex-col items-center'>
+          <h1 className='text-green-500 text-5xl'>{click}</h1>
+          <div className='flex flex-row gap-1'>
+            <Button onClick={() => setClick(click + 1)}>+</Button>
+            <Button onClick={() => setClick(click - 1)}>-</Button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
